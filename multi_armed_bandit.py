c3a208f04cab8949cafb04c830b2622927744f14
Vadym Shturkhal <vadymshturkhal@gmail.com>
2024-03-25 14:42:17 +0200

    Rearrenged some code.

--- multi_armed_bandit.py
+++ multi_armed_bandit.py
@@ -1,15 +1,10 @@
+from unicodedata import name
 import numpy as np
 from agent import BanditAgent
 from game_environment import MultiArmedGame
 
 
-k = 10  # Number of actions (bandits)
-epsilon = 0.1  # Exploration probability
-steps = 1000
-file_path = './bandit_results.csv'
 
-# True reward probabilities for each bandit
-true_reward_probabilities = np.random.rand(k)
 
 # Simulate pulling the bandit's lever
 def bandit(a):
@@ -26,12 +21,21 @@ def train_bandit(game, agent_bandit, steps):
         agent_bandit.update_estimates(action, reward)
 
 
-agent_bandit = BanditAgent(k, epsilon, true_reward_probabilities)
-game = MultiArmedGame(k, true_reward_probabilities, speed=10, is_rendering=True)
+if __name__ =='__main__':
+    k = 10  # Number of actions (bandits)
+    epsilon = 0.1  # Exploration probability
+    steps = 1000
+    file_path = './bandit_results.csv'
 
-train_bandit(game, agent_bandit, steps)
-data = agent_bandit.create_data(file_path=file_path)
-print(data.head(n=k))
+    # True reward probabilities for each bandit
+    true_reward_probabilities = np.random.rand(k)
+
+    agent_bandit = BanditAgent(k, epsilon, true_reward_probabilities)
+    game = MultiArmedGame(k, true_reward_probabilities, speed=60, is_rendering=False)
+
+    train_bandit(game, agent_bandit, steps=1000)
+    data = agent_bandit.create_data(file_path=file_path)
+    print(data.head(n=k))
 
 
 
