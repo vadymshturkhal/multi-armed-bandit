a0604d99f6303de77990e241ec317a8af3dc0914
Vadym Shturkhal <vadymshturkhal@gmail.com>
2024-03-25 20:01:22 +0200

    Added non stationary game start and agent.

---
+++ agent_non_stationary.py
@@ -0,0 +1,51 @@
+import numpy as np
+import pandas as pd
+
+
+class BanditAgent:
+    def __init__(self, k, epsilon, alpha, true_reward_probabilities):
+        self.k = k
+        self.epsilon = epsilon
+        self.true_reward_probabilities = true_reward_probabilities
+        self.alpha = alpha
+
+        # Initialize estimates of action values and action counts
+        self.Q = np.zeros(k)
+        self.N = np.zeros(k)
+        self.sigma = np.zeros(k)  # For the unbiased constant-step-size
+        self.action_count = np.zeros(k)
+
+    # Choose an action using epsilon-greedy strategy
+    def choose_action(self):
+        if np.random.rand() < self.epsilon:
+            return np.random.choice(len(self.Q))  # Explore: choose a random action
+        else:
+            return np.argmax(self.Q)  # Exploit: choose the best current action
+
+    # Update the estimates of action values
+    def update_estimates(self, action, reward):
+        # Update the unbiased constant-step-size parameter
+        self.sigma[action] += self.alpha * (1 - self.sigma[action])
+        # Calculate the step size
+        beta = self.alpha / self.sigma[action]
+        # Update the estimate
+        self.Q[action] += beta * (reward - self.Q[action])
+        self.action_count[action] += 1
+
+    def create_data(self, file_path):
+        # Creating a DataFrame to hold the results
+        results = pd.DataFrame({
+            'Action': np.arange(1, self.k+1),
+            'Estimated Action Values': self._clip_values(self.Q),
+            'True Action Values': self._clip_values(self.true_reward_probabilities),
+            'Number of Times Chosen': self.N
+        })
+
+        # Save the DataFrame to a CSV file
+        results.to_csv(file_path, index=False)
+        return results
+
+    def _clip_values(self, values: list) -> list:
+        for i in range(len(values)):
+            values[i] = f'{values[i]:.4f}'
+        return values
---
+++ non_stationary_multi_armed_bandit.py
@@ -0,0 +1,59 @@
+import numpy as np
+from agent_stationary import BanditAgent
+from game_environment import MultiArmedGame
+import matplotlib.pyplot as plt
+
+
+# Simulate pulling the bandit's lever
+def bandit(a):
+    return 1 if (np.random.rand() < true_reward_probabilities[a]) else 0
+
+def change_arm_probabilities(true_reward_probabilities):
+     true_reward_probabilities += np.random.normal(0, 0.01, size=true_reward_probabilities.shape)
+
+def train_bandit(game, agent_bandit, steps):
+    # Let's simulate 1000 steps of the bandit problem
+    optimal_action_count = np.zeros(steps)
+    for step in range(steps):
+        action = agent_bandit.choose_action()
+        game.apply_action(action)
+        game.play_step()
+        reward = bandit(action)
+        agent_bandit.update_estimates(action, reward)
+
+        if action == optimal_action:
+            optimal_action_count[step] = 1
+        
+        change_arm_probabilities(true_reward_probabilities)
+
+    return np.cumsum(optimal_action_count) / np.arange(1, steps + 1)
+
+# Define a function to plot the results
+def plot_learning_curve(percentages, epsilon):
+    plt.figure(figsize=(10, 5))
+    # plt.plot(range(steps), percentages, label='ε-greedy')
+    plt.plot(percentages, label=f'Realistic, ε-greedy (ε={epsilon})')
+    plt.xlabel('Steps')
+    plt.ylabel('% Optimal Action')
+    plt.title('Percentage of Optimal Actions Taken Over Time')
+    plt.legend()
+    plt.show()
+
+if __name__ =='__main__':
+    k = 10  # Number of actions (bandits)
+    epsilon = 0.1  # Exploration probability
+    steps = 1000
+    file_path = './bandit_results.csv'
+
+    # True reward probabilities for each bandit
+    true_reward_probabilities = np.random.rand(k)
+    optimal_action = np.argmax(true_reward_probabilities)
+
+    agent_bandit = BanditAgent(k, epsilon, true_reward_probabilities)
+    game = MultiArmedGame(k, true_reward_probabilities, speed=60, is_rendering=False)
+
+    percentage_optimal_action = train_bandit(game, agent_bandit, steps=1000)
+    data = agent_bandit.create_data(file_path=file_path)
+    print(data.head(n=k))
+
+    plot_learning_curve(percentage_optimal_action, epsilon)
