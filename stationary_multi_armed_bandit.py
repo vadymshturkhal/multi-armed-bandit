cd6c3f162913357463d1f33e060e2e512102ae47
Vadym Shturkhal <vadymshturkhal@gmail.com>
2024-03-26 12:00:13 +0200

    Removed plot, adapted to use game environment.

--- stationary_multi_armed_bandit.py
+++ stationary_multi_armed_bandit.py
@@ -1,38 +1,18 @@
 import numpy as np
+
 from agent_stationary import BanditAgent
 from game_environment import MultiArmedGame
-import matplotlib.pyplot as plt
 
 
-# Simulate pulling the bandit's lever
-def bandit(a):
-    return 1 if (np.random.rand() < true_reward_probabilities[a]) else 0
-
-def train_bandit(game, agent_bandit, steps):
+def train_bandit(game: MultiArmedGame, agent_bandit: BanditAgent, steps=1000):
     # Let's simulate 1000 steps of the bandit problem
-    optimal_action_count = np.zeros(steps)
-    for step in range(steps):
+    for _ in range(steps):
         action = agent_bandit.choose_action()
-        game.apply_action(action)
+        reward = game.apply_action(action)
+        print(reward)
         game.play_step()
-        reward = bandit(action)
         agent_bandit.update_estimates(action, reward)
 
-        if action == optimal_action:
-            optimal_action_count[step] = 1
-
-    return np.cumsum(optimal_action_count) / np.arange(1, steps + 1)
-
-# Define a function to plot the results
-def plot_learning_curve(percentages, epsilon):
-    plt.figure(figsize=(10, 5))
-    # plt.plot(range(steps), percentages, label='ε-greedy')
-    plt.plot(percentages, label=f'Realistic, ε-greedy (ε={epsilon})')
-    plt.xlabel('Steps')
-    plt.ylabel('% Optimal Action')
-    plt.title('Percentage of Optimal Actions Taken Over Time')
-    plt.legend()
-    plt.show()
 
 if __name__ =='__main__':
     k = 10  # Number of actions (bandits)
@@ -40,15 +20,9 @@ if __name__ =='__main__':
     steps = 1000
     file_path = './bandit_results.csv'
 
-    # True reward probabilities for each bandit
-    true_reward_probabilities = np.random.rand(k)
-    optimal_action = np.argmax(true_reward_probabilities)
+    agent_bandit = BanditAgent(k, epsilon)
+    game = MultiArmedGame(k, speed=60, is_rendering=False)
 
-    agent_bandit = BanditAgent(k, epsilon, true_reward_probabilities)
-    game = MultiArmedGame(k, true_reward_probabilities, speed=60, is_rendering=False)
-
-    percentage_optimal_action = train_bandit(game, agent_bandit, steps=1000)
-    data = agent_bandit.create_data(file_path=file_path)
+    train_bandit(game, agent_bandit, steps)
+    data = agent_bandit.create_data(file_path, game.rewards.true_reward_probabilities)
     print(data.head(n=k))
-
-    # plot_learning_curve(percentage_optimal_action, epsilon)
