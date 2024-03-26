bf23827b56d62b61476f08ed44f0b63e20a3a1be
Vadym Shturkhal <vadymshturkhal@gmail.com>
2024-03-26 15:01:14 +0200

    Hide rewards.

--- rewards.py
+++ rewards.py
@@ -3,12 +3,12 @@ import numpy as np
 
 class Rewards:
     def __init__(self, k:int):
-        self.true_reward_probabilities = np.random.rand(k)
+        self._true_reward_probabilities = np.random.rand(k)
 
     # Simulate pulling the bandit's lever
     def get_reward(self, action):
         return 1 if (np.random.rand() < self.true_reward_probabilities[action]) else 0
 
     def change_reward_probabilities(self):
-        self.true_reward_probabilities += np.random.normal(0, 0.01, size=self.true_reward_probabilities.shape)
-        self.true_reward_probabilities = np.clip(self.true_reward_probabilities, 0, 1)
+        self._true_reward_probabilities += np.random.normal(0, 0.01, size=self._true_reward_probabilities.shape)
+        self._true_reward_probabilities = np.clip(self._true_reward_probabilities, 0, 1)
