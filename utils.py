17cdee339ca471fd910ce6ce1e32d4bf8a4ff5e6
Vadym <vadymshturkhal@gmail.com>
2025-09-03 09:51:45 +0300

    Changed bandits naming from 0 to k-1

--- utils.py
+++ utils.py
@@ -5,7 +5,7 @@ import numpy as np
 def create_bar_data(file_path: str, k: int, agent, true_reward_probabilities):
     # Creating a DataFrame to hold the results
     results = pd.DataFrame({
-        'Action': np.arange(1, k+1),
+        'Action': np.arange(0, k),
         'Estimated Action Values': agent.Q,
         'True Action Values': true_reward_probabilities,
         'Number of Times Chosen': agent.N,
