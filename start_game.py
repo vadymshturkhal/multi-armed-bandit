ba474d2897ebc4283be3a54269894c1725fc88c6
Vadym <vadymshturkhal@gmail.com>
2025-09-03 09:52:31 +0300

    Added logger for agent.

--- start_game.py
+++ start_game.py
@@ -1,16 +1,23 @@
 from agent import Agent
 from game_environment import MultiArmedGame
-from settings import stationary_bandit_data_bar_filename
+from settings import stationary_bandit_data_bar_filename, stationary_bandit_data_average_reward
 from utils import create_bar_data
 
 
 def train_bandit(game: MultiArmedGame, agent_bandit: Agent, steps=1000):
-    # Let's simulate 1000 steps of the bandit problem
-    for _ in range(steps):
-        action = agent_bandit.choose_action()
-        reward = game.apply_action(action)
-        game.play_step()
-        agent_bandit.update_estimates(action, reward)
+    with open(stationary_bandit_data_average_reward, "w") as f:
+        f.write('Step, Action, Reward, Q[action], Average action reward, Average general reward\n')
+        # Let's simulate 1000 steps of the bandit problem
+        for step in range(steps):
+            action = agent_bandit.choose_action()
+            reward = game.apply_action(action)
+            game.play_step()
+            agent_bandit.update_estimates(action, reward)
+
+            # logger
+            average_action_reward = agent_bandit.action_rewards[action] / agent_bandit.N[action]
+            average_general_reward = sum(agent_bandit.action_rewards) / sum(agent_bandit.N)
+            f.write(f'{step}, {action}, {reward}, {agent_bandit.Q[action]:.4f}, {average_action_reward:.4f}, {average_general_reward:.4f}\n')
     
 
 if __name__ =='__main__':
