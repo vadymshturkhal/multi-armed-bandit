2b274741e2e38e3daa01b33afbdf4a4c418d06b0
Vadym <vadymshturkhal@gmail.com>
2025-09-06 20:54:12 +0300

    Changed filepaths for data

--- settings.py
+++ settings.py
@@ -1,2 +1,2 @@
-stationary_bandit_data_bar_filename = './data/stationary_bandit_bar_results.csv'
-stationary_bandit_data_average_reward = './data/stationary_bandit_average_reward.csv'
+stationary_bandit_brief_run_data_filename = './data/stationary_bandit_brief_run_data.csv'
+stationary_bandit_detailed_run_data_filename = './data/stationary_bandit_detailed_run_data.csv'
