7b46653edfc15ba7099b3e13d8885d488a5d42b6
Vadym <vadymshturkhal@gmail.com>
2025-09-07 22:22:17 +0300

    Use RunDataLogger in start_game

--- start_game.py
+++ start_game.py
@@ -1,23 +1,22 @@
 from agent import Agent
 from game_environment import MultiArmedGame
-from settings import stationary_bandit_data_average_reward
-from plot_data import plot_violin_plot
+from settings import stationary_bandit_detailed_run_data_filename
+from utils import RunDataLogger
 
 
-def train_bandit(game: MultiArmedGame, agent_bandit: Agent, steps=1000):
-    with open(stationary_bandit_data_average_reward, "w") as f:
-        f.write('Step,Action,Reward,Q[action],Average action reward,Average general reward\n')
-        for step in range(steps):
-            action = agent_bandit.choose_action()
-            reward = game.apply_action(action)
-            game.play_step()
-            agent_bandit.update_estimates(action, reward)
+def train_bandit(game: MultiArmedGame, agent_bandit: Agent, run_data_logger: RunDataLogger, steps=1000):
+    for step in range(steps):
+        action = agent_bandit.choose_action()
+        reward = game.apply_action(action)
+        game.play_step()
+        agent_bandit.update_estimates(action, reward)
 
-            # logger
-            average_action_reward = agent_bandit.action_rewards[action] / agent_bandit.N[action]
-            average_general_reward = sum(agent_bandit.action_rewards) / sum(agent_bandit.N)
-            f.write(f'{step},{action},{reward:.4f},{agent_bandit.Q[action]:.4f},{average_action_reward:.4f},{average_general_reward:.4f}\n')
-    
+        # logger
+        average_action_reward = agent_bandit.action_rewards[action] / agent_bandit.N[action]
+        average_general_reward = sum(agent_bandit.action_rewards) / sum(agent_bandit.N)
+
+        run_data_logger.add_step_data(step, action, reward, agent_bandit.Q[action], average_action_reward, average_general_reward)
+
 
 if __name__ =='__main__':
     k = 10  # Number of actions (bandits)
@@ -26,7 +25,7 @@ if __name__ =='__main__':
 
     agent_bandit = Agent(k, epsilon)
     game = MultiArmedGame(k, speed=60, is_rendering=False)
+    run_data_logger = RunDataLogger(game.rewards.q_star)
 
-    train_bandit(game, agent_bandit, steps)
-
-    game.rewards.plot_combined(stationary_bandit_data_average_reward)
+    train_bandit(game, agent_bandit, run_data_logger, steps)
+    run_data_logger.create_detailed_run_data(stationary_bandit_detailed_run_data_filename)
