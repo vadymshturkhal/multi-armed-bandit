6e4b456ae66c68e7d3bddd2b70b4efff94640765
Vadym <vadymshturkhal@gmail.com>
2025-09-07 22:17:34 +0300

    Changed data filenames

--- plot_data.py
+++ plot_data.py
@@ -1,12 +1,12 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-from settings import stationary_bandit_data_bar_filename, stationary_bandit_data_average_reward
+from settings import stationary_bandit_brief_run_data_filename, stationary_bandit_detailed_run_data_filename
 
 
 def plot_stationary_bar_data():
     # Load the results from a CSV file
-    df = pd.read_csv(stationary_bandit_data_bar_filename)
+    df = pd.read_csv(stationary_bandit_brief_run_data_filename)
 
     # Create the plot
     plt.figure(figsize=(10, 6))
@@ -28,7 +28,7 @@ def plot_stationary_bar_data():
 
 def plot_violin_plot():
     # Load your CSV
-    df = pd.read_csv(stationary_bandit_data_average_reward)
+    df = pd.read_csv(stationary_bandit_detailed_run_data_filename)
 
     # Group rewards by actionc
     rewards_by_action = [df[df["Action"] == a]["Reward"].values for a in sorted(df["Action"].unique())]
@@ -60,8 +60,6 @@ def plot_violin_plot():
     plt.show()
 
 
-
 if __name__ == '__main__':
     # plot_stationary_bar_data()
     plot_violin_plot()
-
