2da90d794decef790bd847d15f164870ca72e960
Vadym <vadymshturkhal@gmail.com>
2025-09-08 22:46:58 +0300

    Created filenames for epsilon and gradient agents

--- settings.py
+++ settings.py
@@ -1,2 +1,4 @@
 stationary_bandit_brief_run_data_filename = './data/stationary_bandit_brief_run_data.csv'
 stationary_bandit_detailed_run_data_filename = './data/stationary_bandit_detailed_run_data.csv'
+gradient_agent_data_filename = './data/gradient_agent_data.csv'
+epsilon_agent_data_filename = './data/epsilon_agent_data.csv'
